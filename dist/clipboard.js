347419100afbffae73559027d4f9182f2dafa0f5
C凉凉D <cd234350150@outlook.com>
2020-07-14 13:50:51 +0800

    Revert "Prevents scroll jump on iOS when using "data-clipboard-text" Thanks to @geraldarthur at #369"
    This reverts commit a9936ccd233e54017d775d6cd2db787c853c6aaf.

--- dist/clipboard.js
+++ dist/clipboard.js
@@ -240,18 +240,8 @@ function select(element) {
         selectedText = element.value;
     }
     else if (element.nodeName === 'INPUT' || element.nodeName === 'TEXTAREA') {
-        var isReadOnly = element.hasAttribute('readonly');
-
-        if (!isReadOnly) {
-            element.setAttribute('readonly', '');
-        }
-
-        element.select();
+        element.focus();
         element.setSelectionRange(0, element.value.length);
-
-        if (!isReadOnly) {
-            element.removeAttribute('readonly');
-        }
 
         selectedText = element.value;
     }
@@ -462,6 +452,7 @@ module.exports = E;
                 this.fakeElem.style[isRTL ? 'right' : 'left'] = '-9999px';
                 // Move element to the same position vertically
                 var yPosition = window.pageYOffset || document.documentElement.scrollTop;
+                this.fakeElem.addEventListener('focus', window.scrollTo(0, yPosition));
                 this.fakeElem.style.top = yPosition + 'px';
 
                 this.fakeElem.setAttribute('readonly', '');

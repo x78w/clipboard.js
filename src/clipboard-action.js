11a27d2cc673d63608e37cf688df0b8f04ae6539
C凉凉D <cd234350150@outlook.com>
2020-07-14 13:50:51 +0800

    Revert "Fixes #320"
    This reverts commit 03eb05931d414dd36eee612a76fdb7653a990a68.

--- src/clipboard-action.js
+++ src/clipboard-action.js
@@ -63,10 +63,7 @@ class ClipboardAction {
         this.fakeElem.style.position = 'absolute';
         this.fakeElem.style[ isRTL ? 'right' : 'left' ] = '-9999px';
         // Move element to the same position vertically
-        let yPosition = window.pageYOffset || document.documentElement.scrollTop;
-        this.fakeElem.addEventListener('focus', window.scrollTo(0, yPosition));
-        this.fakeElem.style.top = yPosition + 'px';
-
+        this.fakeElem.style.top = (window.pageYOffset || document.documentElement.scrollTop) + 'px';
         this.fakeElem.setAttribute('readonly', '');
         this.fakeElem.value = this.text;
 
